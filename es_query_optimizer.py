f07f6ef7723780b6fc5d63ab105ded3e792b73ab
Davide Setti <davide.setti@gmail.com>
2020-05-11 15:24:44 +0200

    first version

---
+++ es_query_optimizer.py
@@ -0,0 +1,213 @@
+import json
+import hashlib
+import logging
+from collections import defaultdict
+from typing import List, Optional
+
+logger = logging.getLogger(__name__)
+
+
+class Node:
+    def __init__(self, q):
+        self._q = q
+
+    def query(self) -> dict:
+        return self._q
+
+
+class BoolNode(Node):
+    filter: List[Node]
+    must: List[Node]
+    must_not: List[Node]
+    should: List[Node]
+
+    def __init__(self, filter, must, must_not, should):
+        self.filter = filter
+        self.must = must
+        self.must_not = must_not
+        self.should = should
+
+    def total_len(self):
+        return len(self.filter) + len(self.must) + len(self.must_not) + len(self.should)
+
+    def query(self):
+        q = {}
+
+        for field, name in [
+            (self.filter, "filter"),
+            (self.must, "must"),
+            (self.must_not, "must_not"),
+            (self.should, "should"),
+        ]:
+            if field:
+                q[name] = [child.query() for child in field]
+
+        return {"bool": q}
+
+
+class TermsNode(Node):
+    field: Optional[str] = None
+    boost: Optional[str] = None
+
+    def __init__(self, q: dict):
+        super().__init__(q)
+
+        copy_q = dict(q)
+        self.boost = copy_q.pop("boost", None)
+
+        keys = list(copy_q.keys())
+        if len(keys) != 1:
+            logger.warning("Cannot identify field for terms query %v", q)
+            return
+
+        self.field = keys[0]
+        self.values = q[self.field]
+
+        # terms lookup, don't merge
+        # TODO: test
+        if isinstance(self.values, dict):
+            self.field = None
+            return
+
+        _parameters = dict(q)
+        _parameters.pop(self.field)
+        self._parameters = _parameters
+        self._parameters_hash = hashlib.md5(
+            json.dumps(_parameters).encode("utf-8")
+        ).digest()
+
+    def can_be_merged(self, other: "TermsNode") -> bool:
+        return self._parameters == other._parameters
+
+    def mergeability_hash(self) -> str:
+        return f"{self.field}:{self._parameters_hash}"
+
+    def query(self) -> dict:
+        return {"terms": self._q}
+
+
+class QueryParser:
+    @classmethod
+    def parse(cls, q: dict) -> Node:
+        if not q:
+            return Node({})
+
+        if "bool" in q:
+            b = q["bool"]
+            if b.get("_name"):
+                # cannot optimize bool with _name
+                return Node(q)
+            return BoolNode(
+                cls.iter_parse(b.get("filter", [])),
+                cls.iter_parse(b.get("must", [])),
+                cls.iter_parse(b.get("must_not", [])),
+                cls.iter_parse(b.get("should", [])),
+            )
+        if "terms" in q:
+            return TermsNode(q["terms"])
+        else:
+            return Node(q)
+
+    @classmethod
+    def iter_parse(cls, iter_queries) -> List[Node]:
+        return [cls.parse(item) for item in iter_queries]
+
+
+def _optimize_pass(node: Node) -> Node:
+    if isinstance(node, BoolNode):
+        filter_out = []
+        for child in node.filter:
+            if not isinstance(child, BoolNode):
+                filter_out.append(child)
+                continue
+
+            filter_out.extend(child.filter)
+            child.filter = []
+
+            filter_out.extend(child.must)
+            child.must = []
+
+            # remove child if empty
+            if child.total_len():
+                filter_out.append(child)
+        node.filter = filter_out
+
+        must_out = []
+        for child in node.must:
+            if not isinstance(child, BoolNode):
+                must_out.append(child)
+                continue
+
+            must_out.extend(child.must)
+            child.must = []
+
+            node.must_not.extend(child.must_not)
+            child.must_not = []
+
+            # remove child if empty
+            if child.total_len():
+                must_out.append(child)
+        node.must = must_out
+
+        # optimize terms
+        for bool_clause in [node.filter, node.must, node.must_not, node.should]:
+            groupable_terms = defaultdict(list)
+            rewrite = []
+
+            for child in bool_clause:
+                if not isinstance(child, TermsNode):
+                    rewrite.append(child)
+                    continue
+
+                groupable_terms[child.mergeability_hash()].append(child)
+
+            for _, terms_list in groupable_terms.items():
+                rewrite.append(merge_terms(terms_list))
+
+            # overwrite list
+            bool_clause.clear()
+            bool_clause.extend(rewrite)
+
+        # recurse
+        for bool_clause in [node.filter, node.must, node.must_not, node.should]:
+            rewrite = []
+            for child in bool_clause:
+                optimized_child = _optimize_pass(child)
+                rewrite.append(optimized_child)
+            bool_clause.clear()
+            bool_clause.extend(rewrite)
+
+    return node
+
+
+def merge_terms(terms_list: List[TermsNode]) -> TermsNode:
+    first = terms_list[0]
+    if len(terms_list) == 1:
+        return first
+
+    values = []
+    for other in terms_list:
+        values.extend(other.values)
+    parameters = dict(first._parameters)
+    parameters[first.field] = values
+    return TermsNode(parameters)
+
+
+def optimize(q: dict) -> dict:
+    node = QueryParser.parse(q)
+    query = node.query()
+
+    iterations = 100
+    while iterations > 0:
+        iterations -= 1
+
+        node_new = _optimize_pass(node)
+        query_new = node_new.query()
+
+        if query_new == query:
+            return query
+
+        query = query_new
+        node = node_new
+
+    return query
---
+++ test_optimize.py
@@ -0,0 +1,45 @@
+from es_query_optimizer import optimize
+
+
+def test_bool_filter_filter():
+    opt = optimize(
+        {"bool": {"filter": [{"bool": {"filter": [{"exists": {"field": "field1"}}]}}]}}
+    )
+    assert opt == {"bool": {"filter": [{"exists": {"field": "field1"}}]}}
+
+
+def test_bool_filter_filter_10_times():
+    q = {"exists": {"field": "field1"}}
+    for _ in range(10):
+        q = {"bool": {"filter": [q]}}
+    opt = optimize(q)
+    assert opt == {"bool": {"filter": [{"exists": {"field": "field1"}}]}}
+
+
+def test_bool_filter_must():
+    opt = optimize(
+        {"bool": {"filter": [{"bool": {"must": [{"exists": {"field": "field1"}}]}}]}}
+    )
+    assert opt == {"bool": {"filter": [{"exists": {"field": "field1"}}]}}
+
+
+def test_bool_must_must_not():
+    q1 = {"exists": {"field": "field1"}}
+    q = {
+        "bool": {"must": [{"bool": {"must_not": [q1]}}, {"bool": {"must_not": [q1]}},]}
+    }
+    opt = optimize(q)
+    assert opt == {"bool": {"must_not": [q1, q1]}}
+
+
+def test_bool_must_not_terms():
+    q = {
+        "bool": {
+            "must": [
+                {"bool": {"must_not": [{"terms": {"field1": ["01.11"]}}]}},
+                {"bool": {"must_not": [{"terms": {"field1": ["02.22"]}}]}},
+            ]
+        }
+    }
+    opt = optimize(q)
+    assert opt == {"bool": {"must_not": [{"terms": {"field1": ["01.11", "02.22"]}}]}}
