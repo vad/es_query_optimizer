b846977866cafcff95d44c7febd328bd895e5e7d
Stella <stella.margonar@gmail.com>
2020-06-12 11:42:24 +0200

    Bugfix terms merge in Must e Filter (#2)
    do not merge terms query if inside a must/filter
    
    Co-authored-by: Stella Margonar <margonar@spaziodati.eu>

--- es_query_optimizer/optimizer.py
+++ es_query_optimizer/optimizer.py
@@ -151,13 +151,15 @@ def _optimize_pass(node: Node) -> Node:
                 must_out.append(child)
         node.must = must_out
 
+        bool_clauses = {'filter': node.filter, 'must': node.must, 'must_not': node.must_not, 'should': node.should}
+
         # optimize terms
-        for bool_clause in [node.filter, node.must, node.must_not, node.should]:
+        for bool_type, bool_clause in bool_clauses.items():
             groupable_terms = defaultdict(list)
             rewrite = []
 
             for child in bool_clause:
-                if not isinstance(child, TermsNode) or not child.is_mergeable():
+                if bool_type in {'must', 'filter'} or not isinstance(child, TermsNode) or not child.is_mergeable():
                     rewrite.append(child)
                     continue
                 groupable_terms[child.mergeability_hash()].append(child)
@@ -170,7 +172,7 @@ def _optimize_pass(node: Node) -> Node:
             bool_clause.extend(rewrite)
 
         # recurse
-        for bool_clause in [node.filter, node.must, node.must_not, node.should]:
+        for bool_clause in bool_clauses.values():
             rewrite = []
             for child in bool_clause:
                 optimized_child = _optimize_pass(child)
